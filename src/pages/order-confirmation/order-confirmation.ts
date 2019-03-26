96d786b369a295588b18c9c2f91e02b565c80944
Vinicius Lemos <vlemos@gmail.com>
2019-03-26 17:51:26 -0300

    Salvando Pedido

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -6,6 +6,7 @@ import { CartService } from '../../services/domain/cart.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { EnderecoDTO } from '../../models/enderecos.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service';
 
 
 @IonicPage()
@@ -23,7 +24,8 @@ export class OrderConfirmationPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public cartService : CartService,
-    public clienteService: ClienteService) {
+    public clienteService: ClienteService,
+    public pedidoService : PedidoService) {
     this.pedido = this.navParams.get('pedido');
   }
 
@@ -47,6 +49,25 @@ export class OrderConfirmationPage {
     let position = list.findIndex(x => x.id == id);
     return list[position];
 
+  }
+
+  checkout(){
+    this.pedidoService.insert(this.pedido)
+    .subscribe(response => {
+      this.cartService.createOrClearCart();
+      console.log(response.headers.get('location'));
+
+
+    },
+    error => {
+      if(error.status == 403){
+        this.navCtrl.setRoot('HomePage');
+      }
+    });
+  }
+
+  back(){
+    this.navCtrl.setRoot('CartPage');
   }
 
 }
